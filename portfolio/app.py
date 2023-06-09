a52f73da3bb67da435e1fdfcb1c68cf3fe27738b
Vaibhav Kumar <vaibhav.kumar@knoldus.com>
2023-06-09 11:20:25 +0530

    resolving start time error

--- portfolio/app.py
+++ portfolio/app.py
@@ -6,7 +6,6 @@ import logging
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
 
-# app = Flask(__name__, template_folder='portfolio/templates')
 app = Flask(__name__)
 
 # Load environment variables from .env file
@@ -15,9 +14,8 @@ load_dotenv()
 # Configure logging
 logging.basicConfig(filename='logs/app.log', level=logging.INFO)
 
-
 # Define the path to the data directory
-DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../data')
+DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
 
 # Define the path to the report CSV file
 REPORT_FILE = os.path.join(DATA_DIR, 'report_data.csv')
@@ -26,11 +24,10 @@ REPORT_FILE = os.path.join(DATA_DIR, 'report_data.csv')
 FEEDBACK_FILE = os.path.join(DATA_DIR, 'feedback.csv')
 
 # Function to save visitor feedback
-def save_feedback(name, email, feedback, ip_address):
+def save_feedback(name, email, feedback, ip_address, mac_address, visit_time, time_spent, visited_page):
     with open(FEEDBACK_FILE, 'a', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow([name, email, feedback, ip_address])
-
+        writer.writerow([name, email, feedback, ip_address, mac_address, visit_time, time_spent, visited_page])
 
 # Function to read data from a file
 def read_file(filename):
@@ -50,7 +47,7 @@ def get_total_visitors():
 def get_visitors():
     visitors = []
     try:
-        with open(FEEDBACK_FILE, 'r') as file:
+        with open(REPORT_FILE, 'r') as file:
             reader = csv.reader(file)
             next(reader)  # Skip the header row
             for row in reader:
@@ -65,7 +62,7 @@ def get_visitors():
         pass
     return visitors
 
-# Function to get the list of all visitors
+# Function to get the list of all visitors' feedback
 def get_visitors_feedback():
     visitors_feedbacks = []
     try:
@@ -77,7 +74,7 @@ def get_visitors_feedback():
                     'name': row[0],
                     'email': row[1],
                     'feedback': row[2],
-                    'ip_address':row[3]
+                    'ip_address': row[3]
                 })
     except FileNotFoundError:
         pass
@@ -103,7 +100,7 @@ def calculate_time_spent(start_time):
 # Route for the portfolio page
 @app.route('/')
 def portfolio():
-    #Directly Save Visitors Data
+    # Save Visitor's Data
     with open(REPORT_FILE, 'a', newline='') as file:
         ip_address = request.remote_addr
         mac_address = get_mac_address()
@@ -111,7 +108,8 @@ def portfolio():
         time_spent = calculate_time_spent(app.start_time)
         visited_page = request.url
         writer = csv.writer(file)
-        writer.writerow([ip_address,mac_address,visit_time,time_spent,visited_page])
+        writer.writerow([ip_address, mac_address, visit_time, time_spent, visited_page])
+
     try:
         resume_data = read_file('resume.txt')
         journey_data = read_file('journey.txt')
@@ -120,10 +118,10 @@ def portfolio():
         logging.exception('Error occurred while rendering portfolio page: %s', str(e))
         return 'An error occurred. Please try again later.', 500
 
-# Route for submitting and seeing feedback
-@app.route('/feedback', methods=['POST', "GET"])
+# Route for submitting and viewing feedback
+@app.route('/feedback', methods=['POST', 'GET'])
 def submit_feedback():
-    if request.method == "POST":
+    if request.method == 'POST':
         try:
             name = request.form.get('name')
             email = request.form.get('email')
@@ -139,7 +137,7 @@ def submit_feedback():
         except Exception as e:
             logging.exception('Error occurred while submitting feedback: %s', str(e))
             return 'An error occurred. Please try again later.', 500
-    elif request.method == "GET":
+    elif request.method == 'GET':
         try:
             total_visitors = get_total_visitors()
             visitors_feedbacks = get_visitors_feedback()
@@ -159,7 +157,5 @@ def report():
         logging.exception('Error occurred while rendering reports page: %s', str(e))
         return 'An error occurred. Please try again later.', 500
 
-
 if __name__ == '__main__':
-    app.start_time = datetime.datetime.now()
     app.run(host='0.0.0.0', port=5000, debug=True)
